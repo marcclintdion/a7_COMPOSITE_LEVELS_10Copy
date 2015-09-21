e0f932a5fd97a95969dbb195bf729b6feddf10cc
Marc Clint Dion <marcclintdion@Marcs-iMac.local>
2015-09-21 08:03:36 -0700

    init

---
+++ settings.cpp
@@ -0,0 +1,18 @@
+var          adjustShadowPos[]      =  {-0.0004, 0.0004};
+
+var         eyePosition[]    =  {0, 0, -11.157, 0};
+
+var          secondsElapsed        =  6.519;
+
+var          shadow_BIAS                    =  0.006114;
+
+var          adjustShadow_PosRadial_OCC[]      =  {-0.00109, 0};
+
+var         marc_POSITION[]    =  {0.38, -0.79, -8.97695, 1};
+
+var         eyePosition_SHADOW[]    =  {-1.32213, -0.980399, -2.6953, 0};
+
+var           eyePosition[]      =  {0, 0, -11.157, 0.0};
+var           rotateModelWithMiddleMouse[]       =  {0, 0};
+
+var           moveSet[]       =  {0, 0, 0, 1.0};
